c5e47c373ca545e9687f9424e26a530c3e51ba53
Jean-Marc Goepfert <jeanmarcgoepfert@factor5.com.au>
2019-05-08 09:28:31 +1000

    Add more tests to exitablePrompt

--- app/utils/input.test.js
+++ app/utils/input.test.js
@@ -1,13 +1,41 @@
 const { expect } = require("chai");
+const sinon = require("sinon");
 const { exitablePrompt, rl } = require("./input");
 
 describe("exitablePrompt", () => {
+  before(() => {
+    sinon.stub(process, "exit");
+  });
+
+  after(() => {
+    process.exit.restore();
+  });
+
   it("resolves with input provided by user", async () => {
     const input = "answer!";
 
     setTimeout(() => rl.write(input + "\r"), 0);
-    const answer = await asyncPrompt("question? \n");
+
+    const answer = await exitablePrompt("question? \n");
 
     expect(input).to.equal(answer);
+  });
+
+  it("trims the user input", async () => {
+    const input = "answer!";
+
+    setTimeout(() => rl.write(" " + input + " " + "\r"), 0);
+
+    const answer = await exitablePrompt("question? \n");
+
+    expect(input).to.equal(answer);
+  });
+
+  it("should exit on exit on exit command", async () => {
+    setTimeout(() => rl.write("exit" + "\r"), 0);
+
+    await exitablePrompt("question? \n");
+
+    expect(process.exit.called).to.be.true;
   });
 });
