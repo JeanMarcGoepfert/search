9b8cc2da4ed0aec635874ed368fabbccb37043ac
Jean-Marc Goepfert <jeanmarcgoepfert@factor5.com.au>
2019-05-08 15:51:22 +1000

    Split up related and result ui

---
+++ app/cli/messages/related.js
@@ -0,0 +1,65 @@
+const chalk = require("chalk");
+const ui = require("./ui");
+
+function organizations(organizations) {
+  console.log(chalk.bold("Organization:"));
+  organizations.forEach(org => console.log(`${org.name}`));
+}
+
+function submittedTickets(tickets) {
+  console.log(chalk.bold("Submitted Tickets: "));
+  tickets.forEach(ticket => console.log(`${ticket.subject}`));
+}
+
+function assignedTickets(tickets) {
+  console.log(chalk.bold("Assigned Tickets:"));
+  tickets.forEach(ticket => console.log(`${ticket.subject}`));
+}
+
+function submitter(users) {
+  console.log(chalk.bold("Submitted by:"));
+  users.forEach(user => console.log(`${user.name}`));
+}
+
+function assignee(users) {
+  console.log(chalk.bold("Assigned to:"));
+  users.forEach(user => console.log(`${user.name}`));
+}
+
+function users(users) {
+  console.log(chalk.bold("Users:"));
+  users.forEach(user => console.log(`${user.name}`));
+}
+
+function tickets(tickets) {
+  console.log(chalk.bold("Tickets:"));
+  tickets.forEach(ticket => console.log(`${ticket.subject}`));
+}
+
+function assignee(users) {
+  console.log(chalk.bold("Assigned to:"));
+  users.forEach(user => console.log(`${user.name}`));
+}
+
+const relatedFieldsMap = {
+  organizations,
+  submittedTickets,
+  assignedTickets,
+  submitter,
+  assignee,
+  users,
+  tickets
+};
+
+function print(related) {
+  ui.emptyLine();
+  for (key in related) {
+    const handler = relatedFieldsMap[key];
+    if (handler) {
+      handler(related[key]);
+      ui.emptyLine();
+    }
+  }
+}
+
+module.exports = { print };
--- app/cli/messages/result.js
+++ app/cli/messages/result.js
@@ -1,22 +1,15 @@
 const ui = require("./ui");
+const relatedUI = require("./related");
 
 function message({ results, keys, field, value, nsTaken, model }) {
   ui.emptyLine();
   results.forEach(({ row, related }) => {
     ui.sectionBreak();
     keys.forEach(key => ui.row({ keys, key, row, field }));
-    relatedData(related);
+    relatedUI.print(related);
   });
 
   ui.searchMeta({ model, field, value, results, nsTaken });
-}
-
-function relatedData(related) {
-  ui.emptyLine();
-  for (key in related) {
-    ui.related[key](related[key]);
-    ui.emptyLine();
-  }
 }
 
 module.exports = {
